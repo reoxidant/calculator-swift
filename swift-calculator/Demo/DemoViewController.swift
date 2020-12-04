4d3d8b51b520e803dd20bf97645779be13d734bc
Vitaliy Shapovalov <kupper133@gmail.com>
2020-12-05 01:26:27 +0300

    #2 issue update
    Create demo code by lecture 1

---
+++ swift-calculator/Demo/DemoViewController.swift
@@ -0,0 +1,14 @@
+//
+//  DemoViewController.swift
+//  swift-calculator
+//
+//  Created by Виталий Шаповалов on 05.12.2020.
+//  Copyright © 2020 Виталий Шаповалов. All rights reserved.
+//
+
+import UIKit
+
+class DemoViewController: UIViewController {
+
+
+}
