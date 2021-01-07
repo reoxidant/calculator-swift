62a27deecce89e286c71762b15119a1ec0cbd89e
Vitaliy Shapovalov <kupper133@gmail.com>
2021-01-07 18:36:47 +0300

    update dev branch
    Create functions for operate all operation with digits

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,10 +10,20 @@ import UIKit
 
 class ViewController: UIViewController {
 
-
     @IBOutlet weak var display: UILabel!
     
     var typeInTheMiddleOfNumber:Bool = false
+    
+    var operateStack = Array<Double>()
+    
+    var displayValue: Double{
+        get{
+            return (display.text! as NSString).doubleValue
+        }
+        set{
+            display.text = "\(newValue)"
+        }
+    }
     
     @IBAction func digitsPressed(_ sender: UIButton) {
         let digit = sender.currentTitle!
@@ -25,5 +35,32 @@ class ViewController: UIViewController {
             typeInTheMiddleOfNumber = true
         }
     }
+    
+    @IBAction func enter() {
+        typeInTheMiddleOfNumber = false
+        operateStack.append(displayValue)
+        print("operateStack - \(operateStack)")
+    }
+    
+    @IBAction func operate(_ sender: UIButton) {
+        let operation = sender.currentTitle!
+        
+        if(typeInTheMiddleOfNumber){enter()}
+        
+        switch operation{
+            case "÷": performOperation {$1 / $0}
+            case "×": performOperation {$0 * $1}
+            case "-": performOperation {$1 - $0}
+            case "+": performOperation {$0 + $1}
+            default: break;
+        }
+        print(operation)
+    }
+    
+    func performOperation(operation:(Double, Double) -> Double){
+        if operateStack.count >= 2{
+            displayValue = operation(operateStack.removeLast(), operateStack.removeLast())
+            enter()
+        }
+    }
 }
-
